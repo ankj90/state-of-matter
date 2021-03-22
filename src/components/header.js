cbf34f70529033ac7ba3f6e9e6f6905bfe0072dc
sangeet <sangeet.khatri@protonmail.com>
2021-03-22 20:58:41 +0530

    Fixed broken header image

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StaticImage } from "gatsby-plugin-image"
+// import { StaticImage } from "gatsby-plugin-image"
 // import Image from 'next/image';
 
 const Header = ({ content }) => {
   return (
     <header className="relative overflow-hidden flex-shrink-0 text-white">
       <div className="p-12 flex flex-col z-10 relative">{content}</div>
-      <StaticImage src="../images/header-bg.png" alt="Header Background Image" className="absolute inset-0 h-full w-full object-cover z-0" />
+      <img src={'/header-bg.png'} alt="Header Background Image" className="absolute inset-0 h-full w-full object-cover z-0" />
     </header>
   );
 };
