c6d0f5d37bb108a419beb64ee0157931831f15bb
Sangeet Khatri <sangeet.khatri@protonmail.com>
2021-04-13 11:57:40 +0530

    Fixed title and icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `State Of Matter`,
+    description: `State Of Matter`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -24,10 +24,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#F30A49`,
+        theme_color: `#F30A49`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.jpeg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,15 @@ import React from "react"
 
 import Header from "./header"
 import Body from "./body"
+import { Helmet } from "react-helmet"
 
 const Layout = ({ children, header, sidebar, body }) => {
   return (
     <div className="flex">
       <div className="h-screen w-full flex flex-col overflow-y-auto">
+        <Helmet
+          title="State Of Matter"
+        ></Helmet>
         <Header content={header} />
         <Body sidebar={sidebar} body={body} />
       </div>
